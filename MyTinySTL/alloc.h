381143d0910d25f1b1cf657fed3f0b6e5733270c
shy <2519214640@qq.com>
2020-08-15 00:08:13 +0800

    write more and change FreeList** and int &nblock

--- MyTinySTL/alloc.h
+++ MyTinySTL/alloc.h
@@ -22,6 +22,8 @@ namespace mystl
 	};
 	
 	// 不同内存范围的上调大小
+	// 以EAlign128为例，保证他的内存对齐为8的倍数，有关于pragma pack(n)
+	// 用来对齐已经完成储存的内存块
 	enum 
 	{
 		EAlign128 = 8;
@@ -68,8 +70,8 @@ namespace mystl
 		   static size_t M_freelist_index(size_t bytes);
 		   // 重新填充freelist
 		   static void* M_refill(size_t n);
-		   // 从内存池中取空间给free list 使用，条件不允许时，会调整nblock
-		   static char* M_chunk_alloc(size_t size, size_t &nobj);
+		   // 从内存池中取空间给free list 使用，条件不允许时，会调整nblock-------把对size_t的引用改为对int的引用
+		   static char* M_chunk_alloc(size_t size, int &nobj);
 		   
     };
 	
@@ -92,18 +94,21 @@ namespace mystl
 	// 声明为内联函数，那为什么不直接定义在声明处？
 	inline void* alloc::allocate(size_t n)
 	{
-		FreeList* my_free_list;
+		FreeList** my_free_list;
 		FreeList* result;
 		if (n > static_cast<size_t>(ESmallobjectBytes))
 			return std::malloc(n);
-		my_free_list = free_list[M_freelist_index(n)]
-		result = my_free_list;
+		my_free_list = free_list + M_freelist_index(n);
+		//my_free_list = free_list[M_freelist_index(n)]
+		result = *my_free_list;
+		//result = my_free_list;
 		if (result == nullptr)
 		{
 			void* r = M_refill(M_round_up(n));
 			return r;
 		}
-		my_free_list = result -> next;
+		*my_free_list = result->next;
+		//my_free_list = result -> next;
 		return result;
 	}
 	
@@ -115,14 +120,18 @@ namespace mystl
 			return;
 		}
 		FreeList* q = reinterpret_cast<FreeList*>(p);
-		FreeList* my_free_list;
-		// 这里根据区块大小选择又是什么意思？q->next还比较好理解，
-		// 选择的难道是空指针？？？？？？？？？？？？？？？
-		my_free_list = free_list[M_freelist_index(n)];
-		q->next = my_free_list;
-		// 这个my_free_list有什么用啊？？？？？？？？？？
-		// 不要忘了，deallocate是静态函数，可能是这里存在什么作用
-		my_free_list = q;
+		FreeList** my_free_list;
+		my_free_list = free_list + M_freelist_index(n);
+		q->next = *my_free_list;
+		*my_free_list = q; 
+		// FreeList* my_free_list;
+		// // 这里根据区块大小选择又是什么意思？q->next还比较好理解，
+		// // 选择的难道是空指针？？？？？？？？？？？？？？？
+		// my_free_list = free_list[M_freelist_index(n)];
+		// q->next = my_free_list;
+		// // 这个my_free_list有什么用啊？？？？？？？？？？
+		// // 不要忘了，deallocate是静态函数，可能是这里存在什么作用
+		// my_free_list = q;
 	}
 	
 	inline void* alloc::reallocate(void* p,size_t old_size,size_t new_size)
@@ -132,7 +141,7 @@ namespace mystl
 		return p;
 	}
 	
-	//bytes 对应上调大小
+	//bytes 对应上调到多少的区间
 	inline size_t alloc::M_align(size_t bytes)
 	{
 		if (bytes <= 512)
@@ -150,6 +159,57 @@ namespace mystl
 		return ((bytes + M_align(bytes)-1) & ~ (M_align(bytes)-1));
 	)
 	
+	// 根据区块大小，选择第n个freelist
+	inline size_t alloc::M_freelist_index(size_t bytes)
+	{
+		if (bytes <= 512)
+		{
+			return bytes <= 256 ? bytes <= 128 ?
+                ((bytes+EAlign128-1)/EAlign128-1)
+                : (15+(bytes+EAlign256-129)/EAlign256
+                : (23+(bytes+EAlign512-257)/EAlign512);
+        }
+        return bytes <= 2048 ? bytes <= 128 ?
+		    : (31 + (bytes + EAlign1024 -513) /EAlign1024
+		    : (39 + (bytes + EAlign2048 -1025) /EAlign2048
+		    : (47 + (bytes + EAlign4096 -2049) /EAlign4096;
+	}
+	
+	// 重新填充freelist
+	void* alloc::M_refill(size_t n)
+	{
+             			
+	}
+	
+	char* alloc::M_chunk_alloc(size_t size,int& nblock)   
+	{
+		char *result;
+		size_t need_bytes = size * nblock;
+		size_t pool_bytes = end_free - start_free;
+		
+		if (pool_bytes>=need_bytes)
+		{
+			result = start_free;
+			start_free = result + need_bytes;
+			return result;
+		}
+		else if (pool_bytes>=size)
+		{
+			nblock = pool_bytes / size;
+			need_bytes = size * nblock;
+			result = start_free;
+			start_free = result + need_bytes;;
+			return result;
+		}
+		else
+		{
+			if(pool_bytes>0)
+			{
+				FreeList** my_free_list = free_list + M_freelist_index(pool_bytes);
+				((FreeList*)start_free)->next = *my_free_list;
+				*my_free_list = (FreeList*)start_free;
+			}
+				
 	
 	
 	
